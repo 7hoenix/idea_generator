d658d74da8a365799d3d6d2e20eb726ddace9881
philmphoenix <jpholzmann@gmail.com>
2015-11-11 18:16:59 -0700

    Moves site dashboard html and logic to a component
    As per the react way. I moved over the html into a JS component.

--- app/assets/javascripts/components/_dashboard.js.jsx
+++ app/assets/javascripts/components/_dashboard.js.jsx
@@ -1,40 +1,131 @@
+var Dashboard = React.createClass({
+  getInitialState: function(requested_type) {
+    return {requested_type: requested_type, possibilities: [], left: [], right: []}
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: "/api/v1/possibilities.json",
+      type: "GET",
+      data: { requested_type: "business" },
+      success: function(response) {
+        this.setState({possibilities: response, left: response[0], right: response[1]});
+      }.bind(this)
+    });
+  },
+  nextPossibility: function() {
+    var tempPossibilities = this.state.possibilities
+    var possibility = tempPossibilities.pop();
+    this.setState({possibilities: tempPossibilities});
+    return possibility;
+  },
+  changeLeftPossibility: function() {
+    console.log("I'm back to the truth");
+    this.setState({left: this.nextPossibility()});
+  },
+  changeRightPossibility: function() {
+    this.setState({right: this.nextPossibility()});
+  },
+  changeBothPossibilities: function() {
+    this.setState({left: this.nextPossibility()});
+    this.setState({right: this.nextPossibility()});
+  },
+  render: function() {
+    return (
+      <div className="section" id="dashboard-body">
+        <div className="row center dashboard-card-vertical">
+           <Left possibility={this.state.left} newLeftPossibility={this.changeLeftPossibility} />
+           <Right possibility={this.state.right} newRightPossibility={this.changeRightPossibility} />
+         <div className="col s2">
+         </div>
+       </div>
+       <br/>
+        <Composite left={this.state.left} right={this.state.right} changeBothPossibilities={this.changeBothPossibilities} />
+      </div>
+    )
+  }
+});
 
-<div class="section" id="dashboard-body">
-  <div class="row center dashboard-card-vertical">
-   <div class="col s3 offset-s2">
-     <div class="card-content white-text">
-       <h3>Wonder</h3>
-       <p>Source: http://twitter.com/trending</p>
+var Left = React.createClass({
+  handleChangeLeft: function() {
+    this.props.newLeftPossibility();
+  },
+  render: function() {
+    console.log(this.props.possibility);
+     return (
+       <div className="left-possibility">
+         <div className="col s3 offset-s2 left-possibility">
+           <div className="card-content white-text">
+             <h3>{this.props.possibility.title}</h3>
+             <p>Source: {this.props.possibility.source}</p>
+           </div>
+           <div className="card-action">
+            <button className="waves-effect waves-light light-blue btn dashboard-possibility-button"
+                    id="new-possibility-left"
+                    onClick={this.handleChangeLeft}>
+                    <i className="material-icons left"> replay </i>
+                    New possibility
+            </button>
+            <a className="waves-effect waves-light light-blue btn"><i className="material-icons left">thumb_down</i>Blacklist</a>
+           </div>
+         </div>
+       </div>
+     )
+  }
+});
+
+var Right = React.createClass({
+  handleChangeRight: function() {
+    this.props.newRightPossibility();
+  },
+  render: function() {
+    return (
+      <div className="right-possibility">
+         <div className="col offset-s2 s3">
+           <div className="card-content white-text">
+             <h3>{this.props.possibility.title}</h3>
+             <p>Source: {this.props.possibility.source}</p>
+           </div>
+           <div className="card-action">
+            <button className="waves-effect waves-light light-blue btn dashboard-possibility-button"
+                    id="new-possibility-right"
+                    onClick={this.handleChangeRight}>
+                    <i className="material-icons left">
+                    replay
+                    </i>New possibility
+            </button>
+            <a className="waves-effect waves-light light-blue btn"><i className="material-icons left">thumb_down</i>Blacklist</a>
+           </div>
+         </div>
+       </div>
+    )
+  }
+});
+
+var Composite = React.createClass({
+  handleChangeBothPossibilities: function() {
+    this.props.changeBothPossibilities();
+  },
+  render: function() {
+    return (
+    <div className="composite">
+     <div className="row center">
+       <div className="offset-s4 col s4" id="dashboard-combined">
+         <h2>{this.props.left.title} {this.props.right.title}</h2>
+       </div>
+       <div className="s4">
+       </div>
      </div>
-     <div class="card-action">
-      <a class="waves-effect waves-light light-blue btn dashboard-possibility-button"><i class="material-icons left">replay</i>New possibility</a>
-      <a class="waves-effect waves-light light-blue btn"><i class="material-icons left">thumb_down</i>Blacklist</a>
-     </div>
-   </div>
-   <div class="col offset-s2 s3">
-     <div class="card-content white-text">
-       <h3>Woman</h3>
-       <p>Source: http://producthunt.com/new</p>
-     </div>
-     <div class="card-action">
-      <a class="waves-effect waves-light light-blue btn dashboard-possibility-button"><i class="material-icons left">replay</i>New possibility</a>
-      <a class="waves-effect waves-light light-blue btn"><i class="material-icons left">thumb_down</i>Blacklist</a>
-     </div>
-   </div>
-   <div class="col s2">
-   </div>
- </div>
- <br>
- <div class="row center">
-   <div class="offset-s4 col s4" id="dashboard-combined">
-     <h2>Wonder Woman</h2>
-   </div>
-   <div class="s4">
-   </div>
- </div>
- <div class="row center">
-    <a class="waves-effect waves-light light-blue btn"><i class="material-icons left">replay</i>Try again</a>
-    <a class="waves-effect waves-light light-blue btn"><i class="material-icons left">done</i>Save Idea</a>
-  </div>
- <br><br><br><br><br><br><br>
-</div>
+       <div className="row center">
+          <button className="waves-effect waves-light light-blue btn"
+                  onClick={this.handleChangeBothPossibilities}>
+                  <i className="material-icons left">
+                  replay
+                  </i>Try again
+          </button>
+          <a className="waves-effect waves-light light-blue btn"><i className="material-icons left">done</i>Save Idea</a>
+        </div>
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      </div>
+    )
+  }
+});
