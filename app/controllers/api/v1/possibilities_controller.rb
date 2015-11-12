0ac80af07a265a8da74ea0a45770e02c23b27376
philmphoenix <jpholzmann@gmail.com>
2015-11-11 18:59:56 -0700

    Adds extra possibilities to api.

--- app/controllers/api/v1/possibilities_controller.rb
+++ app/controllers/api/v1/possibilities_controller.rb
@@ -2,12 +2,16 @@ class Api::V1::PossibilitiesController < ApplicationController
   respond_to :json
 
   def index
-    possibility1 = Possibility.create(title: "wonder", source: "twitter")
-    possibility2 = Possibility.create(title: "woman", source: "producthunt")
-    possibility3 = Possibility.create(title: "water", source: "twitter")
-    possibility4 = Possibility.create(title: "people carrying sticks", source: "producthunt")
-    possibility_presenter = PossibilityPresenter.new([possibility1,
-      possibility2, possibility3, possibility4])
+    possibilities = []
+    possibilities << Possibility.create(title: "wonder", source: "twitter")
+    possibilities << Possibility.create(title: "woman", source: "producthunt")
+    possibilities << Possibility.create(title: "water", source: "twitter")
+    possibilities << Possibility.create(title: "people carrying sticks", source: "producthunt")
+    possibilities << Possibility.create(title: "inner-city", source: "producthunt")
+    possibilities << Possibility.create(title: "yolo", source: "twitter")
+    possibilities << Possibility.create(title: "Cards Against Humanity", source: "producthunt")
+
+    possibility_presenter = PossibilityPresenter.new(possibilities)
     respond_with possibility_presenter.possibilities
   end
 end
