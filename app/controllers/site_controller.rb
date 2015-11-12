9a6b24e3f0706ecf7d787dff7698a54c5a2e970a
philmphoenix <jpholzmann@gmail.com>
2015-11-12 12:38:50 -0700

    Removes this logic as it is no longer doing anything.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,10 +3,6 @@ class SiteController < ApplicationController
   end
 
   def dashboard
-    possibility1 = Possibility.create(title: "wonder", source: "twitter")
-    possibility2 = Possibility.create(title: "woman", source: "producthunt")
-    @possibility_presenter = PossibilityPresenter.new([possibility1,
-      possibility2])
   end
 
   def profile
