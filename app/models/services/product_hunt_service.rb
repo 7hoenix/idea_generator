14640b39295eed3f040b2125d37d675499db37a6
philmphoenix <jpholzmann@gmail.com>
2015-11-12 20:55:37 -0700

    Adds in connection and parse for product hunt service.

--- app/models/services/product_hunt_service.rb
+++ app/models/services/product_hunt_service.rb
@@ -1,5 +1,23 @@
 class Services::ProductHuntService
+  attr_reader :connection
+
+  def initialize
+    @connection = Hurley::Client.new("https://api.producthunt.com")
+    connection.query[:access_token] = ENV["PRODUCT_HUNT_ACCESS_TOKEN"]
+  end
+
   def raw_possibilities
-    { "http://producthunt.com/new" => "asdf asdf asdf" }
+    possibilities = {}
+    posts = parse(connection.get("/v1/posts"))[:posts]
+    posts.each do |post|
+      possibilities[post[:discussion_url]] = post[:tagline]
+    end
+    possibilities
+  end
+
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
