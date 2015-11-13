a3df55d9bb8a16f614f487a8e8cc77465a24c2ac
philmphoenix <jpholzmann@gmail.com>
2015-11-12 20:55:13 -0700

    Now corrrectly returns raw possibilities
    Raw possibilities now correctly come back from product
    hunt.

--- app/models/possibility_finder.rb
+++ app/models/possibility_finder.rb
@@ -8,16 +8,18 @@ class PossibilityFinder
 
   def possibilities
     # prepare(raw_possibilities)
+    raw_possibilities
+    binding.pry
 
-    possibilities = []
-    possibilities << Possibility.create(title: "wonder", source: "twitter")
-    possibilities << Possibility.create(title: "woman", source: "producthunt")
-    possibilities << Possibility.create(title: "water", source: "twitter")
-    possibilities << Possibility.create(title: "people carrying sticks", source: "producthunt")
-    possibilities << Possibility.create(title: "inner-city", source: "producthunt")
-    possibilities << Possibility.create(title: "yolo", source: "twitter")
-    possibilities << Possibility.create(title: "Cards Against Humanity", source: "producthunt")
-    possibilities
+    # possibilities = []
+    # possibilities << Possibility.create(title: "wonder", source: "twitter")
+    # possibilities << Possibility.create(title: "woman", source: "producthunt")
+    # possibilities << Possibility.create(title: "water", source: "twitter")
+    # possibilities << Possibility.create(title: "people carrying sticks", source: "producthunt")
+    # possibilities << Possibility.create(title: "inner-city", source: "producthunt")
+    # possibilities << Possibility.create(title: "yolo", source: "twitter")
+    # possibilities << Possibility.create(title: "Cards Against Humanity", source: "producthunt")
+    # possibilities
   end
 
   private
@@ -25,9 +27,9 @@ class PossibilityFinder
 
   def raw_possibilities
     services.each do |service|
-      raw_possibilities = raw_possibilities.merge(service.raw_possibilities)
+      @raw_possibilities = @raw_possibilities.merge(service.raw_possibilities)
     end
-    raw_possibilities
+    @raw_possibilities
   end
 
   def services
@@ -35,6 +37,7 @@ class PossibilityFinder
   end
 
   def prepare(raw)
+    binding.pry
     [Protector.new, Cleaner.new, Creator.new].each do |worker|
       worker.send(:prepare, self)
     end
