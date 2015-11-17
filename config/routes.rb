ce76a2e6a56205f1ca2105d408eae5030ca1ed0f
philmphoenix <jpholzmann@gmail.com>
2015-11-17 05:58:12 -0700

    Adds supported routes for lists and ideas
    Properly versioned under version 1 of our api is the
    ability to get lists and ideas.

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   post "dashboard", to: "site#create"
   get "profile", to: "site#profile"
   get "/auth/twitter/callback", to: "sessions#create"
-  resources :users, only: [:show]
   namespace :api do
     namespace :v1, defaults: { format: :json } do
+      resources :users, only: [:show]
       resources :possibilities, only: [:index]
+      resources :lists, only: [:create]
+      resources :ideas, only: [:create, :index]
     end
   end
 end
