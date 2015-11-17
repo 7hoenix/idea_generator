ae3ee1dbb09126cda2b8dec48cd60659f474b634
philmphoenix <jpholzmann@gmail.com>
2015-11-17 06:05:41 -0700

    Adds simple cov, webmock and vcr.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,6 @@
+require "simplecov"
+SimpleCov.start "rails"
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
@@ -6,8 +9,16 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'factory_girl_rails'
 require 'support/controller_helpers'
+require 'webmock'
+require 'vcr'
 
 ActiveRecord::Migration.maintain_test_schema!
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
