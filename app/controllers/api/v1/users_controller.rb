799c3103e8817697e77a4b98b41ead2b1392d943
philmphoenix <jpholzmann@gmail.com>
2015-11-17 05:49:31 -0700

    Moves users controller underneath api. Might move back later.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def show
     @user = current_user
     # @user = UserPresenter.new(current_user)
