49b763ef27d817bf69cfb9763ff4894d80f477cd
philmphoenix <jpholzmann@gmail.com>
2015-11-17 05:54:34 -0700

    Refactors landing page into a component for react
    Its not working :(

---
+++ app/assets/javascripts/components/_landing.js.jsx
@@ -0,0 +1,58 @@
+var Landing = React.createClass({
+  getInitialState: function() {
+    return { left: [] }
+  },
+  optionSubmit: function(ideaType) {
+      console.log(ideaType)
+  },
+  render: function() {
+    return (
+      <div className="section no-pad-bot" id="index-banner">
+        <div className="container">
+          <div className="section">
+          </div>
+          <div className="section">
+          </div>
+          <br/><br/>
+          <h1 className="header center orange-text">Need a little inspiration?</h1>
+          <div className="row">
+          <OptionSelect optionSubmit={this.optionSubmit} />
+           <div className="col s2">
+           </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+var OptionSelect = React.createClass({
+  handleOptionSubmit: function() {
+    var ideaType = (React.findDOMNode(this.refs.type).value.trim());
+    debugger;
+    console.log(idea_type)
+
+    this.props.optionSubmit(ideaType);
+  },
+  render: function() {
+     return (
+
+
+      <div className="input-field offset-s2 col s8">
+         <select>
+           <option value="" disabled selected>Choose a category to get started</option>
+           <option value="business">Business idea</option>
+           <option value="product">Product idea</option>
+         </select>
+        <label>We pull ideas from top trending websites and mash them together.</label>
+         <button className="btn waves-effect waves-light"
+                onClick={this.handleOptionSubmit}
+                 name="action"
+                 value={this.selected}>
+                 Go
+           <i className="material-icons right">send</i>
+          </button>
+       </div>
+     )
+   }
+});
