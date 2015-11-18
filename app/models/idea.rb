545cac3db1b1ef73da9de16803d26e9f166917b0
philmphoenix <jpholzmann@gmail.com>
2015-11-17 17:47:23 -0700

    Adds possibilities and composite methods
    Possibilities is to mimic a many to many relationship
    and composite returns an actual idea.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,12 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
   belongs_to :possibility
+
+  def composite
+    possibilities.map(&:title).join(" ")
+  end
+
+  def possibilities
+    [Possibility.find(possibility_alpha_id), Possibility.find(possibility_beta_id)]
+  end
 end
