4cff58806eae874457dbb4b85c8ac0d5665dad47
philmphoenix <jpholzmann@gmail.com>
2015-11-17 21:32:44 -0700

    Memoizes assigning the user id and redirects to profile.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to user_path(user)
+    session[:user_id] ||= user.id
+    redirect_to profile_path
+  end
+
+  def destroy
+    session.clear
+    redirect_to dashboard_path
   end
 end
