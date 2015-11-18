636ba1436184c51da32d90e782e0a139e646b665
philmphoenix <jpholzmann@gmail.com>
2015-11-17 21:31:51 -0700

    Passes current user into possibility finder poro
    In order to reference personal black and white lists for a
    user P finder needs to know which user to get the records for.

--- app/controllers/api/v1/possibilities_controller.rb
+++ app/controllers/api/v1/possibilities_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::PossibilitiesController < ApplicationController
   respond_to :json
 
   def index
-    respond_with PossibilityFinder.new(params[:idea_type]).possibilities
+    respond_with PossibilityFinder.new(params[:idea_type], current_user).possibilities
   end
 end
