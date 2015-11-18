85d8df4004c2d9ecf4c9d2f713d8c4bcac2e8ac5
philmphoenix <jpholzmann@gmail.com>
2015-11-18 10:58:42 -0700

    Adds remove punctuation method in black list
    Now a black list also cleans up the content by removing
    out any punctuation and numbers.

--- app/models/black_list.rb
+++ app/models/black_list.rb
@@ -14,8 +14,14 @@ class BlackList < Global::BlackList
     finder.raw_possibilities.each do |source, content|
       temp = content.pop.split(" ")
       content = content + (temp.uniq - black_list)
-      post_black_list[source] = content
+      post_black_list[source] = remove_punctuation(content)
     end
     post_black_list
+  end
+
+  private
+
+  def remove_punctuation(content)
+    content.map { |phrase| phrase.gsub(/[^a-zA-Z ]*/, "") }
   end
 end
