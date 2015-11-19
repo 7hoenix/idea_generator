bfa9701e35047cc6d569852f0499a4d42a43dd8a
philmphoenix <jpholzmann@gmail.com>
2015-11-18 22:24:50 -0700

    Adds in a check for whether an idea is saved in session
    Now we direct the redirect based on if we have an idea saved up
    or not.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] ||= user.id
-    redirect_to profile_path
+    if session[:idea_params]
+      redirect_to confirm_path
+    else
+      redirect_to profile_path
+    end
   end
 
   def destroy
