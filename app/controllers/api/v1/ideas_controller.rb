1a0f21b5701b92e1f56db3809899cf13b0b85a63
philmphoenix <jpholzmann@gmail.com>
2015-11-18 22:21:36 -0700

    Adds check if a user is logged in and current route
    Now we can get the current idea stored in the session.

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -6,6 +6,22 @@ class Api::V1::IdeasController < ApplicationController
   end
 
   def create
-    binding.pry
+    if current_user
+      alpha = params[:idea][:possibility_alpha_id]
+      beta = params[:idea][:possibility_beta_id]
+      Idea.find_or_create_by(user_id: current_user.id, possibility_alpha_id:
+        alpha, possibility_beta_id: beta)
+      render json: true
+    else
+      session[:idea_params] = params[:idea]
+      render json: { error: "not logged in"}, status: 401
+    end
+  end
+
+  def current
+    possibility_ids = session.fetch(:idea_params)
+    possibilities = possibility_ids.map { |_, possibility_id| Possibility.find(possibility_id) }
+    session.delete(:idea_params)
+    respond_with possibilities
   end
 end
